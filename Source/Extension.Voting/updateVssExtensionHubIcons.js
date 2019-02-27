98b21b70c3372d16316e720cb0f45213e2ed23e6
Mohammad Al-Kamel <Mohammad.Al-Kamel@aitag.com>
2019-02-27 18:30:41 +0100

    Added hub icon and script to fix path of icon based on the $tfx_publisher

---
+++ Source/Extension.Voting/updateVssExtensionHubIcons.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+
+var vssExtensions = JSON.parse(fs.readFileSync('./vss-extension.json', 'utf-8'));
+
+publisherName = process.argv[2];
+if(!publisherName){
+    publisherName = vssExtensions.publisher;
+}
+
+const searchTerm = `.${vssExtensions.id}`;
+const replacement = `${publisherName}`;//.${vssExtensions.id}`;
+
+for(let contribution of vssExtensions.contributions){
+    if(contribution.properties){
+        if(contribution.properties.iconAsset){
+            var str = contribution.properties.iconAsset;
+            contribution.properties.iconAsset = replaceString(str, searchTerm, replacement);//.replace(regex, replacement);
+        }
+        if(contribution.properties._sharedData){
+            for(var i = 0; i < contribution.properties._sharedData.assets.length; i++){
+                var str = contribution.properties._sharedData.assets[i];
+                contribution.properties._sharedData.assets[i] = replaceString(str, searchTerm, replacement);
+            }
+        }
+    }
+}
+
+fs.writeFileSync('./vss-extension.json', JSON.stringify(vssExtensions, null, 2));
+
+function replaceString(str, searchTerm, replaceTerm){
+    var pointPos = str.indexOf(`${searchTerm}`);
+    if(pointPos > -1){
+        str =`${replaceTerm}${str.substring(pointPos)}`
+    }
+    return str;
+}
